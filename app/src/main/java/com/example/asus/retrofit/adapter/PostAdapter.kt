671b2ab342ee941907ff6914193bbd5f3c03d8c7
Rend1x <grishin.f50@gmail.com>
2018-09-20 18:18:00 +0300

    commit

--- app/src/main/java/com/example/asus/retrofit/adapter/PostAdapter.kt
+++ app/src/main/java/com/example/asus/retrofit/adapter/PostAdapter.kt
@@ -1,2 +1,28 @@
 package com.example.asus.retrofit.adapter
 
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.example.asus.retrofit.R
+import com.example.asus.retrofit.model.Post
+
+class PostAdapter(internal val context: Context, internal  val postList: List<Post>): RecyclerView.Adapter<PostViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_post,parent,false)
+        return PostViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return postList.size
+    }
+
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+        holder.postTitle.text = postList[position].title.toString()
+        holder.postContent.text = StringBuilder(postList[position].body.substring(0,20)).append("...").toString()
+        holder.postAuthor.text = postList[position].userId.toString()
+    }
+
+}
--- app/src/main/java/com/example/asus/retrofit/adapter/PostViewHolder.kt
+++ app/src/main/java/com/example/asus/retrofit/adapter/PostViewHolder.kt
@@ -1,4 +1,13 @@
 package com.example.asus.retrofit.adapter
 
-class PostViewHolder {
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import kotlinx.android.synthetic.main.item_post.view.*
+
+class PostViewHolder(itemView:View): RecyclerView.ViewHolder(itemView) {
+
+    val postTitle = itemView.itemTitle
+    val postContent = itemView.itemContent
+    val postAuthor = itemView.itemAuthor
+
 }
